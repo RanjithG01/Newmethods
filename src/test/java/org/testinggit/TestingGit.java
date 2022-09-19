d36a750ac0e698df84c47e08ee0d8f40376df24e
RanjithG01 <meranjith95@gmail.com>
2022-09-19 20:00:39 +0530

    First time push

---
+++ src/test/java/org/testinggit/TestingGit.java
@@ -0,0 +1,20 @@
+package org.testinggit;
+
+public class TestingGit {
+
+	public void tata() {
+
+		System.out.println("harrier");
+	}
+
+	public void mahindra() {
+
+		System.out.println("xuv 700");
+	}
+
+	public void maruthisuzuki() {
+
+		System.out.println("grand vitara");
+
+	}
+}
